33e0d9de2ead1534c6e4eed0e379c2f952b1a82f
Mateus Araújo Monteiro <129124225+MMatteuss@users.noreply.github.com>
2024-05-22 20:42:34 -0300

    update main.js

--- Atividades-IFAL/Curso IFAL/JS/main.js
+++ Atividades-IFAL/Curso IFAL/JS/main.js
@@ -24,11 +24,11 @@ function minhaFuncao() {
 // }
 
 
- /*
+    /*
     var raiz = prompt("Digite numero")
     alert("o dobro é " + Math.sqrt(raiz))
     */
-    
+
     /*
     var a, b;
     var altura;
@@ -40,18 +40,18 @@ function minhaFuncao() {
     alert("Nome "+NOME+" | Altura: "+altura+" | a: "+a+" | b: "+b)
     alert('Nome ${NOME} | Altura: ${altura} | a: ${a} | b: ${b}')
     */
-    
+
     /*
     var nome = prompt("Digite o seu nome:")
     alert("Ola "+nome)
     */
-    
+
     /*
     var num1 = Number(prompt("Digite numero 1º"));
     var num2 = Number(prompt("Digite numero 2º"));
     alert(num1+" + "+num2+" = "+(num1+num2));
     */
-    
+
     /*
     let idade = prompt("Digite a sua idade")
     if(idade >= 18){
@@ -60,11 +60,11 @@ function minhaFuncao() {
         alert("Você é menor de idade!")
     }
     */
-    
+
     /*
     var nome = prompt("Digite o nome do aluno:")
     let media = prompt("Digite a media:")
-    
+
     if(media >= 7){
         alert("O aluno "+nome+", aprovado")
     }
@@ -72,39 +72,193 @@ function minhaFuncao() {
         alert("O aluno "+nome+", reprovado")
     }
     */
-    
+
     /*
     for(let i=0;i<5;i++){
         alert(i)
     }
-    
+
     let i =0
-    
+
     while(i<5){
         alert(i)
         i++
     }
-    
+
     i=0
     do{
         alert(i)
         i++
     }while(i<5)
     */
-    
+
     /*
     document.write("<p>20 a 50:</p>")
     for(let i = 20; i<=50; i++){
         document.write(" "+i)
     }
-    
+
     document.write("<br><br>")
-    
+
     document.write("<p>50 a 20:</p>")
     for(i = 50; i>=20; i--){
         document.write(" "+i)
     }
     */
+
+    /*
+    var semana = 10;
+    switch (semana) {
+    case 1:
+        document.write("Dom")
+    break;
+    case 2:
+        document.write("Seg")
+    break;
+    case 3:
+        document.write("Ter")
+    break;
+    case 4:
+        document.write("Qua")
+    break;
+    case 5:
+        document.write("Qui")
+    break;
+    case 6:
+        document.write("Sex")
+    break;
+    case 7:
+        document.write("Sab")
+    break;
+    default:
+        document.write(`Semana não encontrado ${semana}`)
+    }
+    */
+
+    /*
+    let x, y, op
+    x = Number(prompt("Digite o primeiro valor"))
+    op = prompt("Digite um operação ")
+    y = Number(prompt("Digite o segundo valor"))
+
+    switch(op){
+        case '+':
+            document.write("Soma: "+ (x+y))
+        break;
+        case '-':
+            document.write("Subtração: "+ (x-y))
+        break;
+        case '*':
+            document.write("Multiplicação: "+ (x*y))
+        break;
+        case '/':
+            document.write("Divisão: "+ (x/y))
+        break;
+        default:
+            document.write(`${op}`)
+        break;
+    }
+    */
+
+    /*
+    var v = [5]
+    for(let i=0; i<5; i++){
+        let name 
+        v[i] = i
+    }
+    for(i=0; i<5; i++){
+        document.write(`${v[i]}`)
+    }
+    */
+
+    /*
+    var v = [5][2]
+    for(let i=0; i<5; i++){
+        for(let l=0; l<5; l++){
+        v[i] = i
+    }
+    for(i=0; i<5; i++){
+        document.write(`${v[i]}`)
+    }
+    */
+
+    /*
+    var i, soma =0
+    var altura = new Array() //uma forma de fazer vetor
+    var valor =[] //outra forma de fazer vetor
+
+    var nota1 = []
+    var nota2 = []
+    var nota3 = []
+    */
+
+    /*
+    var altura = new Array()
+
+    altura [0] = 1.7
+    altura [1] = 1.85
+
+    altura [2] = prompt("Digite um valor para a posição 2")
+
+    altura [3] = 1.63
+
+    altura [4] = prompt("Digite um valor para a posição 4")
+
+    document.write(`${altura[3]}`)
+    //for(let i =0; i<=4; i++){
+    //	document.write(`${altura[i]} <br>`)
+    //}
+    */
+
+    /*
+    var sorteio = []
+
+    for(let i=0; i<7; i++){
+        sorteio[i] = Math.floor(Math.random()*101)
+    }
+    alert(sorteio.toString()+"\n\n" + sorteio.join(" - "))
+    */
+
+    /*
+    function mensagem(){
+        document.write("Teste")
+    }
+    mensagem()
+    */
+
+    /*
+    x = Number(prompt("Digite o primeiro valor"))
+    operacao = (prompt("Digite o operador"))
+    y = Number(prompt("Digite o segundo valor"))
+    calcular(x, operacao, y)
+    function calcular(num1, op, num2){
+        switch(op){
+            case '+':
+                document.write("Soma: "+ (x+y))
+            break;
+            case '-':
+                document.write("Subtração: "+ (x-y))
+            break;
+            case '*':
+                document.write("Multiplicação: "+ (x*y))
+            break;
+            case '/':
+                document.write("Divisão: "+ (x/y))
+            break;
+            default:
+                document.write(`${op}`)
+            break;
+        }
+    }
+    */
+function parOuImpa(numero){
+	if(numero %2 ==0){
+    	document.write("Par")
+    }else{
+    	document.write("Impar")
+    }
+}
+parOuImpa(prompt("Digite um valor"))
 
 
 // CADASTRAR
